e05f65ac3bd47467bf84e46dcc5895536d24981e
yf.tan@allsenseww.com <12345678>
2018-09-04 18:29:45 +0800

    重构

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,53 @@
 <template>
-  <div id="workarea" style="height: 100vh; user-select: none">
+  <section>
+    <svg style="position: absolute; left: 0;top: 0;"
+         :width="canvasLayout.width"
+         :height="canvasLayout.height"
+         :viewBox="svgViewbox">
+      <rect v-for="item in shapes"
+            :key="item.id"
+            :x="item.x - item.offsetX * item.scaleX"
+            :y="item.y - item.offsetY * item.scaleY"
+            fill="#DDD"
+            :width="item.width * item.scaleX"
+            :height="item.height * item.scaleY"
+            :transform="`rotate(${item.rotation} ${item.x - item.offsetX * item.scaleX + (item.width * item.scaleX)/2} ${item.y - item.offsetY * item.scaleY + (item.height * item.scaleY)/2})`"
+      />
+    </svg>
+    <div id="workarea" style="height: 100vh; user-select: none"></div>
+  </section>
 
-  </div>
 </template>
 
 <script>
 
   import Konva from 'konva'
 
+  const WIDTH = window.innerWidth
+  const HEIGHT = window.innerHeight
+
   export default {
     name: 'Editor',
     data() {
       return {
+        canvasLayout: {
+          width: WIDTH,
+          height: HEIGHT,
+          x: 0,
+          y: 0,
+          scale: 1,
+        },
         shapes: [{
-          x: 60,
-          y: 60,
+          id: 0,
+          x: 100,
+          y: 100,
           width: 100,
           height: 90,
+          scaleX: 1,
+          scaleY: 1,
+          offsetX: 0,
+          offsetY: 0,
+          rotation: 0,
           strokeWidth: 1,
           stroke: '#F00',
           name: 'rect',
@@ -24,7 +55,15 @@
         }]
       }
     },
-    computed: {},
+    computed: {
+      svgViewbox() {
+        const x = (-this.canvasLayout.x / this.canvasLayout.scale).toFixed(2)
+        const y = (-this.canvasLayout.y / this.canvasLayout.scale).toFixed(2)
+        const w = (this.canvasLayout.width / this.canvasLayout.scale).toFixed(2)
+        const h = (this.canvasLayout.height / this.canvasLayout.scale).toFixed(2)
+        return `${x} ${y} ${w} ${h}`
+      }
+    },
     mounted() {
       var width = window.innerWidth;
       var height = window.innerHeight;
@@ -98,6 +137,11 @@
   body {
     margin: 0;
     padding: 0;
+  }
+
+  #workarea {
+    height: 100vh;
+    user-select: none;
   }
 
   .toolbar {
